558410f49f164ba07e9406f4a7c65beb50f299fd
saurav <ksaurav325@gmail.com>
2020-06-11 19:16:59 +0530

    Quotes

--- js/to.js
+++ js/to.js
@@ -56,3 +56,43 @@ function removeAll(){
     var l=document.getElementsByTagName("ul");
     l[0].innerHTML="";
 }
+
+(function() {
+    const quotes = [
+      {
+        quote:
+          "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
+        author: " Life"
+      },
+      {
+        quote:
+          "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
+        author: "John Kenneth Galbraith"
+      },
+      {
+        quote:
+          "God save me from my friends. I can protect myself from my enemies.",
+        author: "Claude Louis Hector de Villars "
+      },
+      {
+        quote: "The price of anything is the amount of life you exchange for it.",
+        author: "David Thoreau"
+      },
+      {
+        quote:
+          "Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance. ",
+        author: "Charles Lindbergh"
+      },
+      {
+        quote:
+          "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
+        author: " Tyne Daly"
+      }
+    ];
+  const btn=document.getElementById("gnrt_quote");
+  btn.addEventListener("click",function(){
+      let random=Math.floor(Math.random()*quotes.length);
+      document.getElementById("quotes").textContent=quotes[random].quote;
+      document.querySelector(".author").textContent=quotes[random].author;
+  });
+})();
